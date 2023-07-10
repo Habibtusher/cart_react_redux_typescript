86b17bfd4a03135f787cde42b6fe23a20b7113ef
Habibtusher <tusherhrt@gmail.com>
2023-07-10 11:15:16 +0600

    add comments and refetch data

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,8 +1,6 @@
 import ProductReview from '@/components/ProductReview';
 import { Button } from '@/components/ui/button';
 import { useSingleProductQuery } from '@/redux/api/apiSlice';
-import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
@@ -30,7 +28,7 @@ export default function ProductDetails() {
           <Button>Add to cart</Button>
         </div>
       </div>
-      <ProductReview />
+      <ProductReview id={id as string} />
     </>
   );
 }
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000'}),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  tagTypes:['comments'],
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
@@ -9,7 +10,24 @@ export const api = createApi({
     singleProduct: builder.query({
       query: (id) => `/product/${id}`,
     }),
+    postComment: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/comment/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags:[`comments`] 
+    }),
+    getComments: builder.query({
+      query: (id) => `/comment/${id}`,
+      providesTags:['comments']
+    })
   }),
 });
 
-export const { useGetProductsQuery,useSingleProductQuery } = api;
+export const {
+  useGetProductsQuery,
+  useSingleProductQuery,
+  usePostCommentMutation,
+  useGetCommentsQuery
+} = api;
